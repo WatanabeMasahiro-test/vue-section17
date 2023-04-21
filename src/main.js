87cfbfd7562f2b41bf066fd49ee1ae0b13d5f74d
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-04-21 17:32:19 +0900

    セクション17終了

--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+// import router from './router/index'
+// import store from './store/index'
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = 'https://firestore.googleapis.com/v1/projects/vue-section17-11717/databases/(default)/documents';
+/**
+ * ⏬全てのリクエストに同じ設定をつける方法
+ * axios.defaults.headers.common['Authorization'] = 'HereIsToken';
+ * axios.defaults.headers.get['accept'] = 'application/json';
+ * axios.defaults.headers.post['accept'] = 'application/json';
+ *  */
+const interceptorsRequest = axios.interceptors.request.use(
+    config  => {
+        // console.log('interceptors request', config);
+        return config;
+    },
+    error   => {
+        return Promise.reject(error);
+    },
+);
+const interceptorsResponse = axios.interceptors.response.use(
+    response  => {
+        // console.log('interceptors response', response);
+        return response;
+    },
+    error   => {
+        return Promise.reject(error);
+    },
+);
+
+// console.log(interceptorsRequest);
+// console.log(interceptorsResponse);
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
+
+createApp(App)
+    // .use(router)
+    // .use(store)
+.mount('#app')
